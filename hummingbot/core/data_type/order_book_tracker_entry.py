e1947265fac732be3009d763bb835924707881a5
Amol Shirke <amol.shirke@imagnate.com>
2019-11-13 18:15:51 +0530

    Add files for bitroyal exchange connector to use in hummingbot.

--- hummingbot/core/data_type/order_book_tracker_entry.py
+++ hummingbot/core/data_type/order_book_tracker_entry.py
@@ -8,6 +8,7 @@ from hummingbot.market.idex.idex_active_order_tracker import IDEXActiveOrderTrac
 from hummingbot.market.radar_relay.radar_relay_active_order_tracker import RadarRelayActiveOrderTracker
 from hummingbot.market.bamboo_relay.bamboo_relay_active_order_tracker import BambooRelayActiveOrderTracker
 from hummingbot.market.dolomite.dolomite_active_order_tracker import DolomiteActiveOrderTracker
+from hummingbot.market.bitroyal.bitroyal_active_order_tracker import BitroyalActiveOrderTracker
 
 
 class OrderBookTrackerEntry:
@@ -122,6 +123,23 @@ class CoinbaseProOrderBookTrackerEntry(OrderBookTrackerEntry):
 
     @property
     def active_order_tracker(self) -> CoinbaseProActiveOrderTracker:
+        return self._active_order_tracker
+
+class BitroyalOrderBookTrackerEntry(OrderBookTrackerEntry):
+    def __init__(
+        self, symbol: str, timestamp: float, order_book: OrderBook, active_order_tracker: BitroyalActiveOrderTracker
+    ):
+        self._active_order_tracker = active_order_tracker
+        super(BitroyalOrderBookTrackerEntry, self).__init__(symbol, timestamp, order_book)
+
+    def __repr__(self) -> str:
+        return (
+            f"BitroyalOrderBookTrackerEntry(symbol='{self._symbol}', timestamp='{self._timestamp}', "
+            f"order_book='{self._order_book}')"
+        )
+
+    @property
+    def active_order_tracker(self) -> BitroyalActiveOrderTracker:
         return self._active_order_tracker
 
 
