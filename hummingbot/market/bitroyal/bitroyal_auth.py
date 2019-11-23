0efc07890b448727a952c8999784b379cb51784d
Amol Shirke <amol.shirke@imagnate.com>
2019-11-23 19:46:49 +0530

    authentication test

--- hummingbot/market/bitroyal/bitroyal_auth.py
+++ hummingbot/market/bitroyal/bitroyal_auth.py
@@ -2,38 +2,37 @@ import time
 import hmac
 import hashlib
 import base64
+import requests
 from typing import Dict
+
 
 
 class BitroyalAuth:
     """
     Auth class required by Bitroyal API
-    Learn more at https://bitroyalexchange.com/?python#signing-a-message
+    Learn more at https://alphapoint.github.io/slate/#introduction
     """
-    def __init__(self, api_key: str, secret_key: str, passphrase: str):
-        self.api_key = api_key
-        self.secret_key = secret_key
-        self.passphrase = passphrase
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
 
-    def generate_auth_dict(self, method: str, path_url: str, body: str = "") -> Dict[str, any]:
+    def generate_auth_dict(self) -> Dict[str, any]:
         """
         Generates authentication signature and return it in a dictionary along with other inputs
         :return: a dictionary of request info including the request signature
         """
-        timestamp = str(time.time())
-        message = timestamp + method.upper() + path_url + body
-        hmac_key = base64.b64decode(self.secret_key)
-        signature = hmac.new(hmac_key, message.encode('utf8'), hashlib.sha256)
-        signature_b64 = base64.b64encode(bytes(signature.digest())).decode('utf8')
+        #timestamp = str(time.time())
+        #message = timestamp + method.upper() + path_url + body
+        user_pass = "self.username:self.password"
+        #hmac_key = base64.b64decode(self.secret_key)
+        #signature = hmac.new(hmac_key, message.encode('utf8'), hashlib.sha256)
+        pass_b64 = base64.b64encode(user_pass).decode("ascii")
 
         return {
-            "signature": signature_b64,
-            "timestamp": timestamp,
-            "key": self.api_key,
-            "passphrase": self.passphrase,
+            "auth": pass_b64,
         }
 
-    def get_headers(self, method: str, path_url: str, body: str = "") -> Dict[str, any]:
+    def get_headers(self) -> Dict[str, any]:
         """
         Generates authentication headers required by coinbasse
         :param method: GET / POST / etc.
@@ -41,11 +40,12 @@ class BitroyalAuth:
         :param body: request payload
         :return: a dictionary of auth headers
         """
-        header_dict = self.generate_auth_dict(method, path_url, body)
+        header_dict = self.generate_auth_dict()
         return {
-            "CB-ACCESS-SIGN": header_dict["signature"],
-            "CB-ACCESS-TIMESTAMP": header_dict["timestamp"],
-            "CB-ACCESS-KEY": header_dict["key"],
-            "CB-ACCESS-PASSPHRASE": header_dict["passphrase"],
+            "Authorization": "Basic {}".format(header_dict["auth"]),
+            "Content-Length": 0,
             "Content-Type": 'application/json',
         }
+
+r=requests.post("https://apicoinmartprod.alphapoint.com:8443/AP/Authenticate", headers=BitroyalAuth.get_headers())
+print(r)
