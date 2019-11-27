e5b1eba159c50255ec38b770af5a2b1ffb46922b
bitroyalamol <amol.shirke@bitroyalexchange.com>
2019-11-27 04:02:18 +0000

    changs to auth class

--- hummingbot/market/bitroyal/bitroyal_auth.py
+++ hummingbot/market/bitroyal/bitroyal_auth.py
@@ -1,10 +1,5 @@
-import time
-import hmac
-import hashlib
 import base64
-import requests
 from typing import Dict
-
 
 
 class BitroyalAuth:
@@ -12,40 +7,22 @@ class BitroyalAuth:
     Auth class required by Bitroyal API
     Learn more at https://alphapoint.github.io/slate/#introduction
     """
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.password = password
+    def __init__(self, user_name: str, pass_word: str):
+        self.user_name = user_name
+        self.pass_word = pass_word
 
     def generate_auth_dict(self) -> Dict[str, any]:
-        """
-        Generates authentication signature and return it in a dictionary along with other inputs
-        :return: a dictionary of request info including the request signature
-        """
-        #timestamp = str(time.time())
-        #message = timestamp + method.upper() + path_url + body
-        user_pass = "self.username:self.password"
-        #hmac_key = base64.b64decode(self.secret_key)
-        #signature = hmac.new(hmac_key, message.encode('utf8'), hashlib.sha256)
-        pass_b64 = base64.b64encode(user_pass).decode("ascii")
-
-        return {
-            "auth": pass_b64,
-        }
+        pass
 
     def get_headers(self) -> Dict[str, any]:
         """
-        Generates authentication headers required by coinbasse
-        :param method: GET / POST / etc.
-        :param path_url: e.g. "/accounts"
-        :param body: request payload
+        Generates authentication headers required by BIROYAL
         :return: a dictionary of auth headers
         """
-        header_dict = self.generate_auth_dict()
+        user_pass = bytes(self.user_name + ':' + self.pass_word, 'utf8')
+        pass_key = base64.b64encode(user_pass).decode('utf8')
         return {
-            "Authorization": "Basic {}".format(header_dict["auth"]),
-            "Content-Length": 0,
+            "Authorization": "Basic {}".format(pass_key),
+            "Content-Length": "0",
             "Content-Type": 'application/json',
         }
-
-r=requests.post("https://apicoinmartprod.alphapoint.com:8443/AP/Authenticate", headers=BitroyalAuth.get_headers())
-print(r)
